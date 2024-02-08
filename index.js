8ddb2cb2a93e2afc206b90368ed1257abed50a06
Ephenia Katenta <katentaephenia@gmail.com>
2024-02-08 14:34:43 +0200

    first commit

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
-const appSettings = {
+const appSettings = { //my realtime database //
     databaseURL: "https://realtime-database-84b83-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
+// initializing and creation of shopping list data storer//
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
+  
+//fetching elements for implementation or assigning them//
 
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
 
+//adding event listeners and creating a function to execute the event listener//
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
@@ -27,6 +31,8 @@ onValue(shoppingListInDB, function(snapshot) {
     
         shoppingListEl.innerHTML = ""
         
+        //for loop to check if there are items in the buscket to print the key or value in the realtime database if it is empty it should return the else statement//
+
         for (let i = 0; i < itemsArray.length; i++) {
             let currentItem = itemsArray[i]
             let currentItemID = currentItem[0]
@@ -42,19 +48,21 @@ onValue(shoppingListInDB, function(snapshot) {
 function clearShoppingListEl() {
     shoppingListEl.innerHTML = ""
 }
+//function to clear the input area after clicking add to cart//
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
 }
+// function to create or add items to a list whenever the add to cart button is clicked//
 
 function appendItemToShoppingListEl(item) {
     let itemID = item[0]
     let itemValue = item[1]
-    
+    // the new list / items are consoled here//
     let newEl = document.createElement("li")
     
     newEl.textContent = itemValue
-    
+    //function to remove the key and value from the database by it's exact location//
     newEl.addEventListener("click", function() {
         let exactLocationOfItemInDB = ref(database, `shoppingList/${itemID}`)
         
